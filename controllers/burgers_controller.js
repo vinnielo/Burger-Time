3eb78cb21e4206cde779ca2226d164dc7149e7b2
jose Lopez <vinnielo01@gmail.com>
2020-05-30 09:03:43 -0700

    adding the rest of the full stack files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const burger = require("../models/burger.js");
+const router = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,26 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    // See all the burgers
+    all: function(cb) {
+      orm.all("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // Create a new burger on the list
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.create("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    // Update an existing burger
+    update: function(objColVals, condition, cb) {
+      orm.update("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  // Export the database functions for the controller.
+  module.exports = burger;
