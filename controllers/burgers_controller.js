5e7a7fc0d34a886d98965705150fd747a39d6b98
jose Lopez <vinnielo01@gmail.com>
2020-05-30 11:11:03 -0700

    adding partial folder

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,15 +2,44 @@ const express = require("express");
 const burger = require("../models/burger.js");
 const router = express.Router();
 
+router.get("/", function (req, res) {
+    burger.all(function(data) {
+      const hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
 
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function(result) {
+      console.log("Result:", result);
+      // Refresh the main page
+      res.redirect("/");
+    });
+  });  
 
-
-
-
-
-
-
-
+router.put("/api/burgers/:id", function (req, res) {
+    const condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.update({
+      devoured: req.body.devoured
+    }, condition, function (result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
 
 
 
