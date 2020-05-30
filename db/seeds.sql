7421d98bb42187eee75acfbde4395177ca63140e
jose Lopez <vinnielo01@gmail.com>
2020-05-30 08:52:13 -0700

    adding orm structure

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Mega Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('California Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Garden Burger');
